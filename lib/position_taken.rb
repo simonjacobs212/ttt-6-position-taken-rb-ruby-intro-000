8b906743138741734de6c62ed47dfd9e4d7f5661
Simon Jacobs <simonadlerjacobs@gmail.com>
2020-10-20 21:10:42 +0000

    Automatically backed up by Learn

--- lib/position_taken.rb
+++ lib/position_taken.rb
@@ -1,7 +1,3 @@
 def position_taken?(board, index)
-  if board[index] == "X" || board[index] == "O"
-    return true
-  else board[index] == " "
-    return false
-  end
+  return (board[index] == "X" || board[index] == "O")
 end
